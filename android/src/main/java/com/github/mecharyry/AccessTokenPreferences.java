5be7d7320d9193d55ea15616e219a2dbc44de07e
Ryan Feline <ryan.feline@novoda.com>
2014-07-30 13:48:54 +0100

    Edits poorly chosen constant names.

--- android/src/main/java/com/github/mecharyry/AccessTokenPreferences.java
+++ android/src/main/java/com/github/mecharyry/AccessTokenPreferences.java
@@ -8,7 +8,7 @@ import com.github.mecharyry.auth.oauth.AccessToken;
 public class AccessTokenPreferences {
     private static final String PREF_ACCESS_TOKEN = "access_token";
     private static final String PREF_ACCESS_SECRET = "access_secret";
-    private static final String DEFAULT_VALUE = "";
+    private static final String PREF_DEFAULT_VALUE = "";
     private final SharedPreferences preferences;
 
     public static AccessTokenPreferences newInstance(Context context) {
@@ -34,8 +34,8 @@ public class AccessTokenPreferences {
     }
 
     public AccessToken retrieveAccessToken() {
-        String token = preferences.getString(PREF_ACCESS_TOKEN, DEFAULT_VALUE);
-        String secret = preferences.getString(PREF_ACCESS_SECRET, DEFAULT_VALUE);
+        String token = preferences.getString(PREF_ACCESS_TOKEN, PREF_DEFAULT_VALUE);
+        String secret = preferences.getString(PREF_ACCESS_SECRET, PREF_DEFAULT_VALUE);
         return new AccessToken(token, secret);
     }
 
