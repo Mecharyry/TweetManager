382fea47361a05855a27245d0efbe23a62437f3f
Ryan Feline <ryan.feline@novoda.com>
2014-08-07 11:36:14 +0100

    Creates a network connection broadcast receiver.

---
+++ android/src/main/java/com/github/mecharyry/auth/NetworkChangeReceiver.java
@@ -0,0 +1,41 @@
+package com.github.mecharyry.auth;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+public class NetworkChangeReceiver extends BroadcastReceiver {
+
+    private final Callback callback;
+
+    public interface Callback {
+        void networkAvailable();
+
+        void networkUnavailable();
+    }
+
+    public NetworkChangeReceiver(Callback callback) {
+        this.callback = callback;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.e(NetworkChangeReceiver.class.getSimpleName(), "Context: " + context.toString() + "\nIntent: " + intent);
+
+        if (isNetworkAvailable(context)) {
+            callback.networkAvailable();
+            return;
+        }
+        callback.networkUnavailable();
+    }
+
+    private boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+
+        return null != activeNetworkInfo && activeNetworkInfo.isConnectedOrConnecting();
+    }
+}
