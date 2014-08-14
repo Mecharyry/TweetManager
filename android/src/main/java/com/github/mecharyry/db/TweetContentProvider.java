f47f714fab292260a6f55ef029723a9e7c8f1506
Mecharyry <Mecharyry@gmail.com>
2014-08-14 11:00:44 +0100

    Adds basic query to content provider.

--- android/src/main/java/com/github/mecharyry/db/TweetContentProvider.java
+++ android/src/main/java/com/github/mecharyry/db/TweetContentProvider.java
@@ -1,25 +1,73 @@
 package com.github.mecharyry.db;
 
 import android.content.ContentProvider;
+import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.util.Log;
 
 public class TweetContentProvider extends ContentProvider {
 
-    private ExtendedSQLiteOpenHelper sqLiteOpenHelper;
+    public static final String TAG = TweetContentProvider.class.getSimpleName();
+    private ExtendedSQLiteOpenHelper database;
 
-    private static final String AUTHORITY = "com.github.mecharyry.databaseProvider";
+    private static final int TWEETS = 1;
+    private static final int TWEET_ID = 2;
+
+    private static final String AUTHORITY = "com.github.mecharyry.db.databaseProvider";
+
+    private static final String BASE_PATH = "tweet";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY
+            + "/" + BASE_PATH);
+
+    public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE
+            + "/tweet";
+    public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE
+            + "/tweet";
+
+    private static final UriMatcher sURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sURIMatcher.addURI(AUTHORITY, BASE_PATH, TWEETS);
+        sURIMatcher.addURI(AUTHORITY, BASE_PATH + "/#", TWEET_ID);
+    }
 
     @Override
     public boolean onCreate() {
-        sqLiteOpenHelper = new ExtendedSQLiteOpenHelper(getContext());
+        database = new ExtendedSQLiteOpenHelper(getContext());
         return false;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+
+        // Set the table
+        queryBuilder.setTables(TweetTable.TABLE_NAME);
+
+        int uriType = sURIMatcher.match(uri);
+        switch (uriType) {
+            case TWEETS:
+                break;
+            case TWEET_ID:
+                Log.e(TAG, "" + TWEET_ID);
+                Log.e(TAG, uri.toString());
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+
+        SQLiteDatabase db = database.getWritableDatabase();
+        Cursor cursor = queryBuilder.query(db, projection, selection,
+                selectionArgs, null, null, sortOrder);
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return cursor;
     }
 
     @Override
